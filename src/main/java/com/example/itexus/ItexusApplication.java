4fefaa3e67960e8c2176d1979a4427b59d009ead
Vadzim <shylkovad@gmail.com>
2024-07-31 20:55:30 +0300

    new proekt

---
+++ src/main/java/com/example/itexus/ItexusApplication.java
@@ -0,0 +1,13 @@
+package com.example.itexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itexus/ItexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
