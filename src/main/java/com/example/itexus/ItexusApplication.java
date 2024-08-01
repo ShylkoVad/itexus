17030892f919aad5a96be6a9bc55285a86201c6b
ShylkoVad <shylkovad@gmail.com>
2024-08-01 13:52:40 +0300

    доработка основных классов

--- src/main/java/com/example/itexus/ItexusApplication.java
+++ src/main/java/com/example/itexus/ItexusApplication.java
@@ -1,13 +1,34 @@
 package com.example.itexus;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+import org.springframework.core.env.Environment;
 
-@SpringBootApplication
+@Slf4j
+@SpringBootApplication(exclude = {
+        WebSocketServletAutoConfiguration.class,
+        TaskSchedulingAutoConfiguration.class
+})
 public class ItexusApplication {
 
+    private final Environment environment;
+
+    public ItexusApplication(Environment environment) {
+        this.environment = environment;
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(ItexusApplication.class, args);
+        SpringApplication application = new SpringApplication(ItexusApplication.class);
+        Environment env = application.run(args).getEnvironment();
+        log.info("""
+                        ----------------------------------------------------------
+                        \tApplication '{}' is running! Access URLs:
+                        \tLocal: \t\t{}://localhost:{}{}
+                        ----------------------------------------------------------""",
+                env.getProperty("spring.application.name"), "http", env.getProperty("server.port"), "/user");
     }
 
 }
